e19d593ff44a6c146f6490dbbd09632bcdb71798
Vong <scvong12@gmail.com>
2020-06-11 18:40:31 +1000

    Added view lists created by user 1. User can click on each list to go view it directly. 2. Added delete created list functionality

---
+++ assignment3-ios/Components/OwnedListCell.swift
@@ -0,0 +1,24 @@
+//
+//  OwnedListCell.swift
+//  assignment3-ios
+//
+//  Created by Vong Beng on 11/6/20.
+//  Copyright © 2020 Vong Beng. All rights reserved.
+//
+
+import UIKit
+
+class OwnedListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
